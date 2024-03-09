c540706a0642f145cc8283b4e7153f193c07aae7
amitkushwaha156 <amitkkushwaha155@gmail.com>
2024-03-09 11:46:12 +0530

    responsive_crausel

--- responsive_carousel/script.js
+++ responsive_carousel/script.js
@@ -2,8 +2,18 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  e.target.matches('.next') && slider.append(items[0]);
+  e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener('click', activate, false);
+
+// Function to trigger click event on next button after 2 seconds
+function autoClick() {
+  const nextButton = document.querySelector('.next');
+  nextButton.click();
+  setTimeout(autoClick, 3000); // Call autoClick function again after 2 seconds
+}
+
+// Start the auto click loop
+setTimeout(autoClick, 3000);
